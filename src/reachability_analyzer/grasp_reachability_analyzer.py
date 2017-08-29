4ad4319f5875474368a6b62fd0a83234d4670b03
iretiayo <iretibob@gmail.com>
2017-08-29 14:26:37 -0400

    reachability checking done by doing an IK check with collision turned on

--- src/reachability_analyzer/grasp_reachability_analyzer.py
+++ src/reachability_analyzer/grasp_reachability_analyzer.py
@@ -8,6 +8,8 @@ import moveit_commander
 import tf
 import trajectory_msgs
 
+from moveit_msgs.srv import GetPositionIK
+from moveit_msgs.msg import PositionIKRequest
 
 class GraspReachabilityAnalyzer():
     def __init__(self, move_group, planner_id, allowed_planning_time):
@@ -25,20 +27,24 @@ class GraspReachabilityAnalyzer():
         # this is how long we wait for a response from moveit
         self.planner_timeout = allowed_planning_time + 1
 
+        rospy.wait_for_service('compute_ik')
+        self.compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
+
     def _send_pick_request(self, pickup_goal):
 
         self.pick_plan_client.send_goal(pickup_goal)
 
-        received_result = self.pick_plan_client.wait_for_result(
-            rospy.Duration(self.planner_timeout))
+        received_result = self.pick_plan_client.wait_for_result()
 
         if received_result:
             result = self.pick_plan_client.get_result()
             rospy.loginfo("result: " + str(result))
             success = result.error_code.val == result.error_code.SUCCESS
         else:
-            result = None
-            success = False
+            # result = None
+            # success = False
+            rospy.logerr("unable to communicate with movegroup")
+            rospy.signal_shutdown("unable to communicate with movegroup")
 
         rospy.loginfo("success of pick_plan_client:" + str(success))
 
@@ -52,25 +58,50 @@ class GraspReachabilityAnalyzer():
         moveit_grasp_msg = message_utils.graspit_interface_to_moveit_grasp(
             graspit_grasp_msg, object_name)
 
-        pickup_goal = message_utils.build_pickup_goal(
-            moveit_grasp_msg=moveit_grasp_msg,
-            object_name=object_name,
-            allowed_planning_time=self.allowed_planning_time,
-            planner_id=self.planner_id,
-            planning_group=self.move_group)
+        # pickup_goal = message_utils.build_pickup_goal(
+        #     moveit_grasp_msg=moveit_grasp_msg,
+        #     object_name=object_name,
+        #     allowed_planning_time=self.allowed_planning_time,
+        #     planner_id=self.planner_id,
+        #     planning_group=self.move_group)
 
-        success = False
-        result = None
+        # success = False
+        # result = None
+        # try:
+        #     self.pick_plan_client.wait_for_server(rospy.Duration(3))
+
+        #     rospy.loginfo("moveit_grasp_msg: " + str(moveit_grasp_msg))
+        #     rospy.loginfo("pickup_goal: " + str(pickup_goal))
+
+        #     success, result = self._send_pick_request(pickup_goal)
+
+        # except Exception as e:
+        #     rospy.logerr(
+        #         "failed to reach pick action server with err: %s" % e.message)
+
+        # # This section uses direct moveit_commander plan to pose (withough pick_place interface).
+        # target_pose = moveit_grasp_msg.grasp_pose
+        # rospy.loginfo(target_pose)
+        # self.move_group.set_pose_target(target_pose, 'wrist_roll_link')
+        # self.move_group.set_planner_id(self.planner_id)
+        # plan = self.move_group.plan()
+        # success = len(plan.joint_trajectory.points) > 0
+        # result = plan
+
+
+        service_request = PositionIKRequest()
+        service_request.group_name = "arm"
+        service_request.ik_link_name = "wrist_roll_link"
+        service_request.pose_stamped = moveit_grasp_msg.grasp_pose
+        service_request.timeout.secs= self.allowed_planning_time
+        service_request.avoid_collisions = True
+
         try:
-            self.pick_plan_client.wait_for_server(rospy.Duration(3))
+            resp = self.compute_ik(ik_request = service_request)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
 
-            rospy.loginfo("moveit_grasp_msg: " + str(moveit_grasp_msg))
-            rospy.loginfo("pickup_goal: " + str(pickup_goal))
-
-            success, result = self._send_pick_request(pickup_goal)
-
-        except Exception as e:
-            rospy.logerr(
-                "failed to reach pick action server with err: %s" % e.message)
+        success = resp.error_code.val == 1
+        result = resp
 
         return success, result
